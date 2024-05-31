b2a495e2e01279ce799785da0deb2b3811ad594f
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-31 19:38:16 +0330

    add ModelViewSet, PageNumberPagination, CompanySerializer, Company

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from .serializers import CompanySerializer
+from .models import Company
 
 # Create your views here.
