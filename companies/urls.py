1b36e2b3a886c336cd2eb402220cab57af4a7fcc
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-31 19:39:42 +0330

    register company_router

--- companies/urls.py
+++ companies/urls.py
@@ -2,3 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import CompanyViewSet
 
 company_router = DefaultRouter()
+company_router.register(
+    prefix="companies", viewset=CompanyViewSet, basename="companies"
+)
